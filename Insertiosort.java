c3ed077fe45d1ffa3bad6ac624d041e5157c5652
karan pawar <85121133+kdp-alpha@users.noreply.github.com>
2022-11-19 16:43:53 +0530

    Create Insertiosort.java

---
+++ Insertiosort.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {
+            3,
+            5,
+            8,
+            4,
+            1,
+            9,
+            -2
+        };
+        int n = arr.length;
+
+        for (int i = 0; i <= arr.length - 2; i++) {
+            for (int j = i + 1; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
