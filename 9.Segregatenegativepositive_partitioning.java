ca4b3bfe95f1d8d5579d02b52785f6c9934812df
karan pawar <85121133+kdp-alpha@users.noreply.github.com>
2022-11-21 18:40:42 +0530

    Create 9.Segregatenegativepositive_partitioning.java

---
+++ 9.Segregatenegativepositive_partitioning.java
@@ -0,0 +1,37 @@
+public class Main {
+  private static void swap(int[] arr, int i, int j) {
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+  public static void main(String[] args) {
+    int[] arr = {
+      9,
+      -3,
+      5,
+      -2,
+      -8,
+      -6,
+      1,
+      3
+    };
+
+    int pivot = 0;
+    int i = 0;
+    int j = 0;
+
+    while (i < arr.length) {
+      if (arr[i] >= pivot) {
+        i++;
+      } else {
+        swap(arr, i, j);
+        i++;
+        j++;
+      }
+    }
+
+    for (int k = 0; k < arr.length; k++) {
+      System.out.println(arr[k]);
+    }
+  }
+}
